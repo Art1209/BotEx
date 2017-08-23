5d578ed8f5f2e0ebc7ddcbffa1a9dbee1eb50d48
art102 <a5903281@ya.ru>
2017-08-23 15:38:55 +0300

    added controller for heroku

---
+++ src/main/java/service/Config.java
@@ -0,0 +1,11 @@
+package service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("service")
+public class Config {
+}
---
+++ src/main/java/service/MainController.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tlgrm.MainBot;
+
+@Controller
+@RequestMapping("*")
+public class MainController {
+    @RequestMapping(value = "*", method = RequestMethod.GET)
+    @ResponseBody
+    public String enter(){
+        MainBot.main(new String[1]);
+        return "success";
+    }
+}
